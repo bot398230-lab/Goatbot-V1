6837995bd80c05721c9406acfc5695882760fffc
Mirjafor <bot398230@gmail.com>
2026-06-22 15:44:38 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -5,7 +5,7 @@ module.exports = {
         config: {
                 name: "admin",
                 version: "1.6",
-                author: "NTKhang",
+                author: "𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁",
                 countDown: 5,
                 role: 2,
                 description: {
