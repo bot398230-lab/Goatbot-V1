ea928cb0c85e79b55fedee657796d5bec9717b3b
Mirjafor <bot398230@gmail.com>
2026-06-22 15:52:27 +0600

    Update author name in help command configuration

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -6,7 +6,7 @@ module.exports = {
 		name: "help",
 		aliases: ["menu", "commands"],
 		version: "4.8",
-		author: "NeoKEX",
+		author: "𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁",
 		shortDescription: "Show all available commands",
 		longDescription: "Displays a clean and premium-styled categorized list of commands.",
 		category: "system",
